609db47a2d272802caa595e8dc7ee16cb32d10a3
7statum <7statumbh@gmail.com>
2019-01-23 04:11:55 +0300

    mkchange
    chg

--- marker-detect.js
+++ marker-detect.js
@@ -1,14 +1,17 @@
-
-  AFRAME.registerComponent('registerevents', {
+AFRAME.registerComponent('registerevents', {
     init: function () {
+
         var marker = this.el;
         var avideo = document.getElementById('avideo');
 
         marker.addEventListener('markerFound', function () {
+
+        
             var markerId = marker.id;
             // TODO: Add your own code here to react to the marker being found.
             console.log('markerFound', markerId);
             if (markerId == 'marker-hiro') {
+                
                 // avideo.play();
                 // avideo.addEventListener('click', evt => avideo.play());
                 console.log('hiro-video-start')
@@ -29,7 +32,24 @@
             if (markerId == 'marker-kanji') {
                 console.log('lstkanji')
             }
+
         });
-    }
+    },
+    update: function () {
+
+        var animation = { progress: 0 };
+        var tween = new TWEEN.Tween(animation)
+            .to({ progress: 1 }, 1000)
+            .onUpdate(function () {
+                document.querySelector('a-image').setAttribute('sprite-sheet', 'progress', animation.progress);
+            });
+        tween.onComplete(function () { animation.progress = 0;});
+        tween.repeat(Infinity);
+        tween.start();
+    },
+    tick: function () { },
+    remove: function () { },
+    pause: function () { },
+    play: function () { }
 });
 
